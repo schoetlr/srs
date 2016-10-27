cb12446bbcad4bafabb10c14d9576c61829f2000
schoetlr <srlrs20020@gmail.com>
2016-10-27 14:30:02 -0400

    new card form

--- app/assets/javascripts/controllers/new_card_ctrl.js
+++ app/assets/javascripts/controllers/new_card_ctrl.js
@@ -1,6 +1,18 @@
-srs.controller("NewCardCtrl", ['$scope', '_', function($scope, _){
+srs.controller("NewCardCtrl", ['$scope', '_', 'deck', 'deckService', function($scope, _, deck, deckService){
 
+  $scope.cardForm = {};
 
+  deckService.getDecks().then(function(response){
+    $scope.decks = response;
+  })
+
+  $scope.selectedDeck = deck;
+
+  $scope.createCard = function(){
+    cardService.createCard($scope.cardForm);
+
+    $scope.cardForm = {};
+  }
   
 
 
