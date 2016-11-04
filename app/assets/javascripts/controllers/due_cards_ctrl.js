3345bba461d56c011cca0a1dfd8f9ae2e38672e2
schoetlr <srlrs20020@gmail.com>
2016-11-04 12:18:42 -0400

    html markup for study due cards

--- app/assets/javascripts/controllers/due_cards_ctrl.js
+++ app/assets/javascripts/controllers/due_cards_ctrl.js
@@ -15,14 +15,33 @@ srs.controller("DueCardsCtrl", ['$scope', 'timeService', 'cards', '_', function(
   })
 
 
+  $scope.cardSide = 'front';
+
+  $scope.flipCard = function(){
+    if($scope.cardSide === 'front'){
+      $scope.cardSide = 'back';
+    } else {
+      $scope.cardSide = 'front';
+    }
+  };
+
   $scope.nextCard = function(){
-    $scope.card = $scope.cards.shift();
+    //set the next_due for the card based on the response
+
+
+    if($scope.cards.length > 0){
+      $scope.card = $scope.cards.shift();
+    } else {
+      console.log("else");
+      $scope.cardSide = "outOfCards";
+    }
+    
   };
 
 
   $scope.redoCard = function(){
     // set the next_due of the card
-    
+
     $scope.cards.push($scope.card);
     $scope.nextCard();
   };
